885d2e0643b699f2f83953cd291f9289dc0cc19b
MO <masaruo@gmail.com>
2020-08-07 05:15:33 +0900

    最初の機能確認

---
+++ src/dialogs/popup.js
@@ -0,0 +1,16 @@
+(function () {
+    "use strict";
+
+    Office.onReady()
+        .then(function () {
+
+            // TODO1: Assign handler to the OK button.
+            document.getElementById("ok-button").onclick = sendStringToParentPage;
+        });
+
+    // TODO2: Create the OK button handler
+function sendStringToParentPage() {
+    var userName = document.getElementById("name-box").value;
+    Office.context.ui.messageParent(userName);
+}
+}());
